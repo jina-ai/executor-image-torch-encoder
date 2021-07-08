fd4a2d521a133553d2979f310e8845d62a541f62
David Buchaca Prats <davidbuchaca@gmail.com>
2021-07-08 12:34:53 +0200

    fix: traversal paths (#17)
    * fix: traversal paths
    
    * fix: missing traversal_paths
    
    Co-authored-by: Joan Fontanals <joan.martinez@jina.ai>
    
    * fix: traversal paths in test
    
    Co-authored-by: Joan Fontanals <joan.martinez@jina.ai>

--- tests/integration/test_flow_integration.py
+++ tests/integration/test_flow_integration.py
@@ -48,14 +48,14 @@ def test_with_batch():
 
 
 @pytest.mark.parametrize(
-    ['docs', 'docs_per_path', 'traversal_path'],
+    ['docs', 'docs_per_path', 'traversal_paths'],
     [
         (pytest.lazy_fixture('docs_with_blobs'), [['r', 11], ['c', 0], ['cc', 0]], 'r'),
         (pytest.lazy_fixture('docs_with_chunk_blobs'), [['r', 0], ['c', 11], ['cc', 0]], 'c'),
         (pytest.lazy_fixture('docs_with_chunk_chunk_blobs'), [['r', 0], ['c', 0], ['cc', 11]], 'cc')
     ]
 )
-def test_traversal_path(docs: DocumentArray, docs_per_path: List[List[str]], traversal_path: str):
+def test_traversal_paths(docs: DocumentArray, docs_per_path: List[List[str]], traversal_paths: str):
     def validate_traversal(expected_docs_per_path: List[List[str]]):
         def validate(res):
             for path, count in expected_docs_per_path:
@@ -69,7 +69,7 @@ def test_traversal_path(docs: DocumentArray, docs_per_path: List[List[str]], tra
         resp = flow.post(
             on='/test',
             inputs=docs,
-            parameters={'traversal_path': [traversal_path]},
+            parameters={'traversal_paths': [traversal_paths]},
             return_results=True
         )
 
