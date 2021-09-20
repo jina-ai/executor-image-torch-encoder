b318bcd3ee84b38b884cc77cb81d18aa8509b816
Nan Wang <nan.wang@jina.ai>
2021-09-20 16:48:06 +0800

    feat: adapt to the the latest hubble

--- executor/torch_encoder.py
+++ executor/torch_encoder.py
@@ -1,55 +1,56 @@
 __copyright__ = "Copyright (c) 2021 Jina AI Limited. All rights reserved."
 __license__ = "Apache-2.0"
 
-from typing import Optional, List, Dict, Iterable, Tuple
+from typing import Dict, Iterable, List, Optional, Tuple
 
 import numpy as np
-
-import torchvision.transforms as T
 import torch
-
-from jina import Executor, requests, DocumentArray
+import torchvision.transforms as T
+from jina import DocumentArray, Executor, requests
+from jina.logging.logger import JinaLogger
 from jina_commons.batching import get_docs_batch_generator
 
-from models import EmbeddingModelWrapper
+from .models import EmbeddingModelWrapper
 
 
 class ImageTorchEncoder(Executor):
     """
     :class:`ImageTorchEncoder` encodes ``Document`` blobs of type `ndarray` (`float32`) and shape
-    `B x H x W x C` into `ndarray` of `B x D`.
-    Where `B` is the batch size and `D` is the Dimension of the embedding.
-    If `use_default_preprocessing=False`, the expected input shape is `B x C x H x W` with `float32` dtype.
+    `H x W x C` into `ndarray` of shape `D`.
+    Where `D` is the Dimension of the embedding.
+    If `use_default_preprocessing=False`, the expected input shape is `C x H x W` with `float32` dtype.
+
+    :class:`ImageTorchEncoder` fills the `embedding` fields of `Documents` with an `ndarray` of shape `embedding_dim`
+    (size depends on the model) with `dtype=float32`.
 
     Internally, :class:`ImageTorchEncoder` wraps the models from
     `torchvision.models`.
     https://pytorch.org/vision/stable/models.html
-
-    :param model_name: the name of the model. Some of the models:
-        ``alexnet``, `squeezenet1_0``,  ``vgg16``,
-        ``densenet161``, ``inception_v3``, ``googlenet``,
-        ``shufflenet_v2_x1_0``, ``mobilenet_v2``,
-        ``mnasnet1_0``, ``resnet18``. See full list above.
-    :param device: Which device the model runs on. Can be 'cpu' or 'cuda'
-    :param default_traversal_paths: Used in the encode method an defines traversal on the received `DocumentArray`
-    :param default_batch_size: Defines the batch size for inference on the loaded PyTorch model.
-    :param args:  Additional positional arguments
-    :param kwargs: Additional keyword arguments
     """
+
     def __init__(
         self,
         model_name: str = 'resnet18',
-        device: Optional[str] = None,
-        default_traversal_path: Tuple = ('r', ),
+        device: str = 'cpu',
+        default_traversal_path: Tuple = ('r',),
         default_batch_size: Optional[int] = 32,
         use_default_preprocessing: bool = True,
         *args,
-        **kwargs
+        **kwargs,
     ):
+        """
+        :param model_name: the name of the model. Some of the models:
+            ``alexnet``, `squeezenet1_0``,  ``vgg16``,
+            ``densenet161``, ``inception_v3``, ``googlenet``,
+            ``shufflenet_v2_x1_0``, ``mobilenet_v2``,
+            ``mnasnet1_0``, ``resnet18``. See full list above.
+        :param device: Which device the model runs on. Can be 'cpu' or 'cuda'
+        :param default_traversal_paths: Used in the encode method an defines traversal on the received `DocumentArray`
+        :param default_batch_size: Defines the batch size for inference on the loaded PyTorch model.
+        """
         super().__init__(*args, **kwargs)
+        self.logger = JinaLogger(self.__class__.__name__)
 
-        if not device:
-            device = 'cuda' if torch.cuda.is_available() else 'cpu'
         self.device = device
         self.default_batch_size = default_batch_size
         self.use_default_preprocessing = use_default_preprocessing
@@ -59,18 +60,17 @@ class ImageTorchEncoder(Executor):
         # axis 0 is the batch
         self._default_channel_axis = 1
 
-        self.model_wrapper = EmbeddingModelWrapper(model_name)
+        self.model_wrapper = EmbeddingModelWrapper(model_name, device=self.device)
 
-        self._preprocess = T.Compose([
-            T.ToPILImage(),
-            T.Resize(256),
-            T.CenterCrop(224),
-            T.ToTensor(),
-            T.Normalize(
-                mean=[0.485, 0.456, 0.406],
-                std=[0.229, 0.224, 0.225]
-            )
-        ])
+        self._preprocess = T.Compose(
+            [
+                T.ToPILImage('RGB'),
+                T.Resize(256),
+                T.CenterCrop(224),
+                T.ToTensor(),
+                T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+            ]
+        )
 
     @requests
     def encode(self, docs: Optional[DocumentArray], parameters: Dict, **kwargs):
@@ -85,9 +85,11 @@ class ImageTorchEncoder(Executor):
         if docs:
             docs_batch_generator = get_docs_batch_generator(
                 docs,
-                traversal_path=parameters.get('traversal_paths', self.default_traversal_path),
+                traversal_path=parameters.get(
+                    'traversal_paths', self.default_traversal_path
+                ),
                 batch_size=parameters.get('batch_size', self.default_batch_size),
-                needs_attr='blob'
+                needs_attr='blob',
             )
             self._compute_embeddings(docs_batch_generator)
 
